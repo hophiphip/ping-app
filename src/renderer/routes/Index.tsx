9d42048d25ec7628e9cc0d7ea987c4dc1d2ed58c
hophiphip <nighop@yandex.ru>
2022-05-05 22:58:25 +0400

    Index page changes

--- src/renderer/routes/Index.tsx
+++ src/renderer/routes/Index.tsx
@@ -3,23 +3,34 @@ import { useEffect, useState } from 'react';
 import '../css/Index.css';
 
 export default () => {
-  const [message, setMessage] = useState('[no message]');
+  const [isRemote, setIsRemote] = useState(false);
+  const [platform, setPlatform] = useState('');
+  const [timestamp, setTimestamp] = useState('');
 
   const onButtonClick = () => {
     window.electron.ipcRenderer.sendMessage('rdp-test', []);
   }
 
   useEffect(() => {
+    // Request update once on load
+    window.electron.ipcRenderer.sendMessage('rdp-test', []);
+
+    // Listen for responses
     window.electron.ipcRenderer.on('rdp-test', (data) => {
       const values = JSON.parse(String(data));
-      setMessage(`${values.platform}: [${values.timestamp}] ${values.isRdp ? 'rdp session' : 'desktop session'}`);
+
+      setTimestamp(`${values.timestamp}`);
+      setIsRemote(values.isRdp);
+      setPlatform(values.platform);
     });
   }, []);
 
   return (
     <div>
-      <button onClick={onButtonClick}>Send IPC message</button>
-      <p>{message}</p>
+      <button onClick={onButtonClick}>Request HOST status</button>
+      <p><strong>Timestamp:</strong> {timestamp}</p>
+      <p><strong>Platform:</strong> {platform}</p>
+      <p><strong>Session:</strong> {isRemote ? 'remote' : 'local'}</p>
     </div>
   );
 };
